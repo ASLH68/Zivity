95912786054e90dac0f103b293d282114aa6be1e
AndreaXyna <andreaswihartdecoster@gmail.com>
2025-01-09 15:42:06 -0600

    Added custom movement component
    Added custom movement sprint property

---
+++ Source/Zivity/Private/ZV_CharacterMovementComponent.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ZV_CharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UZV_CharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	/** The speed to set when the character is sprinting */
+	UPROPERTY(Category="Character Movement: Walking", EditAnywhere, BlueprintReadWrite, meta=(ClampMin="0", UIMin="0", ForceUnits="cm/s"))
+	float MaxWalkSpeedSprinting;
+};
